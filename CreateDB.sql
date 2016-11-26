ed1280e8b7438a6519db69f4fa1a685dd616ac41
Alexander Tsebenko <sas@mbh.mbhtech>
2016-11-26 19:26:16 +0600

    rename tables to lowercase

--- CreateDB.sql
+++ CreateDB.sql
@@ -4,32 +4,32 @@ CREATE DATABASE lemon_stat
 	CHARACTER SET utf8
 	COLLATE utf8_general_ci;
     
-CREATE TABLE lemon_stat.Groups (
+CREATE TABLE lemon_stat.groups (
   id int(11) NOT NULL AUTO_INCREMENT,
   groupname varchar(255) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE INDEX Groups_uName (groupname)
+  UNIQUE INDEX groups_uName (groupname)
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci
 ROW_FORMAT = DYNAMIC;
 
-CREATE TABLE lemon_stat.Sites (
+CREATE TABLE lemon_stat.sites (
   id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
   groupid int(11) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE INDEX Sites_uName (name,groupid),
-  CONSTRAINT Groups_f3id FOREIGN KEY (groupid)
-  REFERENCES lemon_stat.Groups (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  UNIQUE INDEX sites_uName (name,groupid),
+  CONSTRAINT groups_f3id FOREIGN KEY (groupid)
+  REFERENCES lemon_stat.groups (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci
 ROW_FORMAT = DYNAMIC;
 
-CREATE TABLE lemon_stat.Pages (
+CREATE TABLE lemon_stat.pages (
   id int(11) NOT NULL AUTO_INCREMENT,
   founddatetime datetime DEFAULT NULL,
   lastscandate datetime DEFAULT NULL,
@@ -38,57 +38,57 @@ CREATE TABLE lemon_stat.Pages (
   PRIMARY KEY (id),
   UNIQUE INDEX Pages_uUrl_uSiteId (url, siteid),
   CONSTRAINT Pages_fSiteId FOREIGN KEY (siteid)
-  REFERENCES lemon_stat.Sites (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  REFERENCES lemon_stat.sites (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci
 ROW_FORMAT = DYNAMIC;
 
-CREATE TABLE lemon_stat.Persons (
+CREATE TABLE lemon_stat.persons (
   id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
   groupid int(11) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE INDEX Persons_uName (name,groupid),
-  CONSTRAINT Groups_f2Id FOREIGN KEY (groupid)
-  REFERENCES lemon_stat.Groups (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  UNIQUE INDEX persons_uName (name,groupid),
+  CONSTRAINT groups_f2Id FOREIGN KEY (groupid)
+  REFERENCES lemon_stat.groups (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci
 ROW_FORMAT = DYNAMIC;
 
-CREATE TABLE lemon_stat.Keywords (
+CREATE TABLE lemon_stat.keywords (
   id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
   personid int(11) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE INDEX Keywords_uName_uPersonId (name, personid),
   CONSTRAINT Keywords_fPersonId FOREIGN KEY (personid)
-  REFERENCES lemon_stat.Persons (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  REFERENCES lemon_stat.persons (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci
 ROW_FORMAT = DYNAMIC;
 
-CREATE TABLE lemon_stat.PersonPageRank (
+CREATE TABLE lemon_stat.person_page_rank (
   rank int(11) DEFAULT NULL,
   personid int(11) NOT NULL,
   pageid int(11) NOT NULL,
   PRIMARY KEY (personid, pageid),
   CONSTRAINT PPR_fPageId FOREIGN KEY (pageid)
-  REFERENCES lemon_stat.Pages (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  REFERENCES lemon_stat.pages (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT PPR_fPersonId FOREIGN KEY (personid)
-  REFERENCES lemon_stat.Persons (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  REFERENCES lemon_stat.persons (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci
 ROW_FORMAT = DYNAMIC;
 
-CREATE TABLE lemon_stat.Accounts (
+CREATE TABLE lemon_stat.accounts (
   id int(11) NOT NULL AUTO_INCREMENT,
   username varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
@@ -96,23 +96,23 @@ CREATE TABLE lemon_stat.Accounts (
   groupid int(11) NOT NULL,
   privilege int(11) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE INDEX Account_uName (username,groupid),
-  CONSTRAINT Groups_fId FOREIGN KEY (groupid)
-  REFERENCES lemon_stat.Groups (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  UNIQUE INDEX Account_uName (username),
+  CONSTRAINT groups_fId FOREIGN KEY (groupid)
+  REFERENCES lemon_stat.groups (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci
 ROW_FORMAT = DYNAMIC;
 
-CREATE TABLE lemon_stat.Tokens (
+CREATE TABLE lemon_stat.tokens (
   id int(11) NOT NULL AUTO_INCREMENT,
   token varchar(255) NOT NULL,
   accountid int(11) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE INDEX Tokens_uName (token),
+  UNIQUE INDEX tokens_uName (token),
   CONSTRAINT PPR_fAccountId FOREIGN KEY (accountid)
-  REFERENCES lemon_stat.Accounts (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  REFERENCES lemon_stat.accounts (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
